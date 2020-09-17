1474f4e29d0f65c09bbc22c9d97a2d339a519295
willsn <willsn@gmail.com>
2020-09-16 18:54:12 -0600

    only one model output

--- pages/index.py
+++ pages/index.py
@@ -39,8 +39,8 @@ LSTM_model_inputs = build_LSTM_1_day_model_inputs(weather_forecast_df, current_f
 
 
 
-one_day_forecast = generate_1_day_prediction(model_inputs)
-# one_day_forecast = current_flow
+# one_day_forecast = generate_1_day_prediction(model_inputs)
+one_day_forecast = current_flow
 
 ## Create forecast dataframe
 forecast_data = {
@@ -49,19 +49,19 @@ forecast_data = {
      }
 df = pd.DataFrame(data=forecast_data)
 
-two_seven_day_forecast = generate_2_7_day_prediction(df, weather_forecast_df)
+# two_seven_day_forecast = generate_2_7_day_prediction(df, weather_forecast_df)
 
-# two_seven_day_forecast = {
-#     'Forecast': [352.15, 348.7, 345.95, 375.85, 368.8, 364.2],
-#     'date': ['2020-09-17','2020-09-18','2020-09-19','2020-09-20','2020-09-21','2020-09-22'],
-#      }
-# two_seven_day_forecast = pd.DataFrame(data=two_seven_day_forecast)
+two_seven_day_forecast = {
+    'Forecast': [352.15, 348.7, 345.95, 375.85, 368.8, 364.2],
+    'date': ['2020-09-17','2020-09-18','2020-09-19','2020-09-20','2020-09-21','2020-09-22'],
+     }
+two_seven_day_forecast = pd.DataFrame(data=two_seven_day_forecast)
 # two_seven_day_forecast = two_seven_day_forecast.append(df, ignore_index=True)
 
 prev_flow_df = prev_flow_df.append(two_seven_day_forecast, ignore_index=True)
 # prev_flow_df = prev_flow_df.append(df, ignore_index=True)
 
-forecast_list = prev_flow_df['date'].tail(7).tolist()
+# forecast_list = prev_flow_df['date'].tail(7).tolist()
 # print(forecast_list)
 
 
